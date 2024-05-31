5128d2992b7e8a1b283c41a111fa9628a3b6338d
Ahmed Samir <a.samir9710@gmail.com>
2024-05-31 15:02:09 +0300

    solved binary tree level order traversal II bfs

---
+++ bfs_problems/python/binary_tree_level_order_traversal_II.py
@@ -0,0 +1,29 @@
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        queue = []
+        result = []
+        if root is None:
+            return result
+
+        queue.append(root)
+        while queue:
+            result.append([node.val for node in queue])
+            temp = []
+            for node in queue:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            queue = temp
+        return result[::-1]
